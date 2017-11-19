fdc699aa32de299ab430f272ac0aed79f7364ffd
Alexis Janon <alexis.janon@ensimag.grenoble-inp.fr>
2017-11-19 22:34:53 +0100

    Common functions and variables for easier use of mpi.

---
+++ src/mpi_common.c
@@ -0,0 +1,33 @@
+/******************************************************************************
+* Copyright (C) 2017 Alexis Janon
+* Copyright (C) 2017 Benjamin Roussillon
+* 
+* This code is part of master parallel systems course project at the
+* Université Grenoble Alpes.
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version. 
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+* 
+* You should have received a copy of the GNU General Public License
+* along with this program.  If not, see <http://www.gnu.org/licenses/>.
+******************************************************************************/
+
+#include <mpi.h>
+
+int rank = -1, size = -1;
+
+void mpi_common_init(int argc, char **argv)
+{
+	if(rank > -1 || size > -1)
+		return;
+	MPI_Init(&argc, &argv);
+	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+	MPI_Comm_size(MPI_COMM_WORLD, &size);
+}
---
+++ src/mpi_common.h
@@ -0,0 +1,34 @@
+/******************************************************************************
+* Copyright (C) 2017 Alexis Janon
+* Copyright (C) 2017 Benjamin Roussillon
+* 
+* This code is part of master parallel systems course project at the
+* Université Grenoble Alpes.
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version. 
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+* 
+* You should have received a copy of the GNU General Public License
+* along with this program.  If not, see <http://www.gnu.org/licenses/>.
+******************************************************************************/
+
+#ifndef MPI_COMMON_H
+#define MPI_COMMON_H 1
+
+#include "log.h"
+
+#define MPI_LOG(level, fmt, ...) \
+	log_msg(level, "(%02d) " fmt, rank, __VA_ARGS__)
+
+int rank, size;
+
+void mpi_common_init(int argc, char **argv);
+
+#endif
