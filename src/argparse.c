24fcdcc542291043bb30f2b633e313ed56e3e40a
Alexis Janon <alexis.janon@ensimag.grenoble-inp.fr>
2017-11-19 20:20:31 +0100

    Added argument parsing code. Modified from the GNU manual.
    Contains the appropriate licenses for the use of the GNU manual
    (ie. Free Documentation License).

---
+++ src/argparse.c
@@ -0,0 +1,92 @@
+/******************************************************************************
+* Copyright (C) 2017 Alexis Janon
+* Copyright (C) 2017 Benjamin Roussillon
+* 
+* This code is part of master parallel systems course project at the
+* Université Grenoble Alpes.
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version. 
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+* 
+* You should have received a copy of the GNU General Public License
+* along with this program.  If not, see <http://www.gnu.org/licenses/>.
+******************************************************************************/
+
+#include <argp.h>
+#include "argparse.h"
+
+const char *argp_program_version =
+	"m2r_project_mpi_broadcast 0.1";
+
+/* Program documentation. */
+static char doc[] =
+	"Université Grenoble Alpes parallel systems course project\n\
+This is a student project whose only aim is to better understand the MPI \
+algorithms.\n\n\
+TOPOLOGY may take any of these values:\n\
+    ring";
+
+/* A description of the arguments we accept. */
+static char args_doc[] = "TOPOLOGY";
+
+/* The options we understand. */
+static struct argp_option options[] = {
+	{"verbose",  'v', 0,       0, "Increase verbosity", 0},
+	{"quiet",    'q', 0,       0, "Decrease verbosity", 0},
+	{0}
+};
+
+/* Parse a single option. */
+static error_t parse_opt(int key, char *arg, struct argp_state *state)
+{
+	/* Get the input argument from argp_parse, which we
+   	know is a pointer to our arguments structure. */
+	struct arguments *arguments = state->input;
+
+	switch (key) {
+  	case 'q':
+    		arguments->quiet += 1;
+    		break;
+  	case 'v':
+    		arguments->verbose += 1;
+    		break;
+  	case ARGP_KEY_NO_ARGS:
+    		argp_usage(state);
+  	case ARGP_KEY_ARG:
+		/* Here we know that state->arg_num == 0, since we
+		force argument parsing to end before any more arguments can
+		get here. */
+		arguments->topology = arg;
+
+		/* In addition, by setting state->next to the end
+		of the arguments, we can force argp to stop parsing here and
+		return. */
+		state->next = state->argc;
+
+		break;
+  	default:
+    		return ARGP_ERR_UNKNOWN;
+  	}
+	return 0;
+}
+
+/* Our argp parser. */
+static struct argp argp = {options, parse_opt, args_doc, doc, 0, 0, 0};
+
+int argparse(int argc, char **argv, struct arguments *arguments)
+{
+	/* Default values. */
+	arguments->quiet = 0;
+	arguments->verbose = 0;
+
+	/* Parse our arguments; every option seen by parse_opt will be
+   	reflected in arguments. */
+	return argp_parse(&argp, argc, argv, 0, 0, arguments);
+}
