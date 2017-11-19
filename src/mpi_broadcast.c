92f442bb20fb13f626c7b559526bad01a1543b13
Alexis Janon <alexis.janon@ensimag.grenoble-inp.fr>
2017-11-19 20:21:40 +0100

    First version of the mpi_broadcast test program.
    For now, only prints the chosen topology, and configures
    the log level according to the cli arguments.

---
+++ src/mpi_broadcast.c
@@ -0,0 +1,49 @@
+/******************************************************************************
+* Copyright (C) 2017 Alexis Janon
+* Copyright (C) 2017 Benjamin Roussillon
+* 
+* This code is part of master parallel systems course project at the
+* Université Grenoble Alpes.
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version. 
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+* 
+* You should have received a copy of the GNU General Public License
+* along with this program.  If not, see <http://www.gnu.org/licenses/>.
+******************************************************************************/
+
+#include "argparse.h"
+#include "log.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdint.h>
+#include <string.h>
+
+void update_log_level(struct arguments *arguments)
+{
+	int8_t log_change = arguments->verbose - arguments->quiet;
+	int8_t new_log_level = runtime_log_level + log_change;
+	if(new_log_level >= LOG_FATAL) {
+		runtime_log_level = LOG_FATAL;
+	} else if(new_log_level < LOG_TRACE) {
+		runtime_log_level = LOG_TRACE;
+	} else {
+		runtime_log_level = new_log_level;
+	}
+}
+
+int main(int argc, char **argv)
+{
+	struct arguments arguments;
+	argparse(argc, argv, &arguments);
+	update_log_level(&arguments);
+	log_msg(LOG_INFO, "Toplogy = [%s]", arguments.topology);
+	exit(EXIT_SUCCESS);
+}
