fea50bf1493d460af27713f571d8e7af630f94e8
Alexis Janon <kardyne@gmail.com>
2017-12-04 23:49:35 +0100

    Updated run.sh to handle many new cases

--- run.sh
+++ run.sh
@@ -1,9 +1,143 @@
-#!/bin/sh
+#!/bin/bash
 
-process_count=$1
-topology=$2
-shift; shift
+check_dependency() { command -v $1 >/dev/null 2>&1 || { printf "$1 is required but it is not installed. Aborting.\n"; exit 1; } }
+check_dependencies() {
+	for DEP in "$@"; do
+		check_dependency $DEP
+	done
+}
 
-python ./tools/generate_xml_ring_and_hostfile.py $process_count ./hostfiles/ ./platforms/
+is_integer() {
+	local RE='^[0-9]+$'
+	[[ $1 =~ $RE ]] || return 1
+}
 
-smpirun -np $process_count -platform platforms/"${topology}_${process_count}".xml -hostfile hostfiles/hostfile_"$process_count".txt ./mpi_allreduce $topology $@
+getopt_test() {
+	getopt --test > /dev/null
+	if [[ $? -ne 4 ]]; then
+		printf "'getopt --test' failed in this environment.\n"
+		exit 1
+	fi
+	return 0
+}
+
+getopt_parse() {
+	getopt_test
+	SHORT=w:h:
+	LONG=np:,width:,height:,help
+	getopt --options $SHORT --longoptions $LONG --name "$0" -- "$@"
+	if [[ $? -ne 0 ]]; then
+		printf "getopt could not pass the arguments.\n"
+		exit 2
+	fi
+}
+
+parse_arguments() {
+	help_opt=0
+	eval set -- "$(getopt_parse $@)"
+	while true; do
+		case "$1" in
+			--np)
+                if ! is_integer "$2"; then
+					printf "$2 is not a number\n"
+					exit 1
+				fi
+				process_count="$2"
+				shift 2
+				;;
+            -w|--width)
+                if ! is_integer "$2"; then
+					printf "$2 is not a number\n"
+					exit 1
+				fi
+				width="$2"
+				shift 2
+				;;
+            -h|--height)
+                if ! is_integer "$2"; then
+					printf "$2 is not a number\n"
+					exit 1
+				fi
+				height="$2"
+				shift 2
+				;;
+			--help)
+				help_opt=1
+				shift
+				;;
+			--)
+                if ! is_integer "$2"; then
+					printf "$2 is not a number\n"
+					exit 1
+				fi
+                process_count="$2"
+                topology="$3"
+				shift 3
+				break
+				;;
+			*)
+				printf "Unhandled parsing error\n"
+				exit 3
+				;;
+		esac
+	done
+	check_arguments
+}
+
+check_arguments() {
+	[[ $help_opt -eq 1 ]] && display_usage && exit 0
+    [[ -z "$topology" ]] && printf "Topology is mandatory.\n" && exit 1
+	[[ -z "$process_count" ]] && printf "Process count is mandatory.\n" && exit 1
+    if [[ "$topology" == "grid2d" ]]; then
+        [[ -z "$width" ]] && printf "Width is mandatory for grid topology.\n" && exit 1
+        [[ -z "$height" ]] && printf "Height is mandatory for grid topology.\n" && exit 1
+    fi
+}
+
+print_usage() {
+	printf "Options:\n"
+	printf " --np, <count>\tNumber of processors (mandatory)\n"
+    printf " -w, --width, <num>\tWidth (only for grid)\n"
+    printf " -h, --height, <num>\tHeight (only for grid)\n"
+    printf " -- \tAll options after this will be passed to the mpi_allreduce program\n"
+	printf " --help \tPrint this message\n"
+}
+
+display_usage() {
+	printf "Usage: $(basename $0) topology process_count\n"
+	printf "$(print_usage)" | column -t -s $'\t'
+}
+
+main() {
+    check_dependency python
+    python_bin=python
+    if $python_bin -V | grep -q "Python 3"; then
+        check_dependency python2
+        python_bin=python2
+    fi
+	check_dependencies getopt printf smpirun column
+	parse_arguments $@
+    shift; shift; shift;
+
+    case "$topology" in
+    "ring")
+        $python_bin ./tools/generate_xml_ring_and_hostfile.py $process_count ./hostfiles/ ./platforms/
+        smpirun -np $process_count \
+            -platform "platforms/${topology}_${process_count}.xml" \
+            -hostfile "hostfiles/hostfile_$process_count.txt" \
+            ./mpi_allreduce $topology $@
+        ;;
+    "grid2d")
+        $python_bin ./tools/generate_xml_grid_and_hostfile.py $height $width ./hostfiles/ ./platforms/
+        smpirun -np $process_count \
+            -platform "platforms/grid_${height}_${width}.xml" \
+            -hostfile "hostfiles/grid_hostfile_${height},${width}.txt" \
+            ./mpi_allreduce $topology $@
+        ;;
+    *)
+        printf "Topology %s not implemented. Exiting.\n" "$topology"
+        exit 1
+    esac
+}
+
+main $@
