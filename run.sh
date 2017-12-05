f5b2d4a977d083585e0c8cc54fb844014277b954
Alexis Janon <kardyne@gmail.com>
2017-12-05 17:25:53 +0100

    Updated run script for latency and bandwidth

--- run.sh
+++ run.sh
@@ -23,8 +23,8 @@ getopt_test() {
 
 getopt_parse() {
 	getopt_test
-	SHORT=w:h:d
-	LONG=np:,width:,height:,help
+	SHORT=w:h:dl:b:
+	LONG=np:,width:,height:,help,latency:,bandwidth
 	getopt --options $SHORT --longoptions $LONG --name "$0" -- "$@"
 	if [[ $? -ne 0 ]]; then
 		printf "getopt could not pass the arguments.\n"
@@ -62,6 +62,22 @@ parse_arguments() {
 				height="$2"
 				shift 2
 				;;
+			-l|--latency)
+				if ! is_integer "$2"; then
+					printf "$2 is not a number\n"
+					exit 1
+				fi
+				latency="$2"
+				shift 2
+				;;
+			-b|--bandwidth)
+				if ! is_integer "$2"; then
+					printf "$2 is not a number\n"
+					exit 1
+				fi
+				bandwidth="$2"
+				shift 2
+				;;
 			-d)
 				default_opt=1
 				shift
@@ -71,12 +87,12 @@ parse_arguments() {
 				shift
 				;;
 			--)
-                if ! is_integer "$2"; then
+                if ! is_integer "$3"; then
 					printf "$2 is not a number\n"
 					exit 1
 				fi
-                process_count="$2"
-                topology="$3"
+                topology="$2"
+				process_count="$3"
 				shift 3
 				bin_args="$@"
 				break
@@ -100,6 +116,8 @@ check_arguments() {
         [[ -z "$width" ]] && printf "Width is mandatory for grid topology.\n" && exit 1
         [[ -z "$height" ]] && printf "Height is mandatory for grid topology.\n" && exit 1
     fi
+	[[ -z "$latency" ]] && printf "Latency is mandatory.\n" && exit 1
+	[[ -z "$bandwidth" ]] && printf "Bandwidth is mandatory.\n" && exit 1
 }
 
 print_usage() {
@@ -107,6 +125,8 @@ print_usage() {
 	printf " --np, <count>\tNumber of processors (mandatory)\n"
     printf " -w, --width, <num>\tWidth (only for grid)\n"
     printf " -h, --height, <num>\tHeight (only for grid)\n"
+	printf " -l, --latency, <num>\tLatency\n"
+	printf " -b, --bandwidth, <num>\tBandwidth\n"
 	printf " -d \tUse the default MPI_Allreduce call\n"
     printf " -- \tAll options after this will be passed to the mpi_allreduce program\n"
 	printf " --help \tPrint this message\n"
@@ -129,14 +149,18 @@ main() {
 
     case "$topology" in
     "ring")
-        $python_bin ./tools/generate_xml_ring_and_hostfile.py $process_count ./hostfiles/ ./platforms/
+        $python_bin ./tools/generate_xml_ring_and_hostfile.py \
+			$process_count ./hostfiles/ ./platforms/ \
+			-b "$bandwidth" -l "$latency"
         smpirun -np $process_count \
             -platform "platforms/${topology}_${process_count}.xml" \
             -hostfile "hostfiles/hostfile_$process_count.txt" \
             ./mpi_allreduce $bin_topology -w $width -h $height $bin_args
         ;;
     "grid2d")
-        $python_bin ./tools/generate_xml_grid_and_hostfile.py $height $width ./hostfiles/ ./platforms/
+        $python_bin ./tools/generate_xml_grid_and_hostfile.py \
+			$height $width ./hostfiles/ ./platforms/ \
+			-b "$bandwidth" -l "$latency"
         smpirun -np $process_count \
             -platform "platforms/grid_${height}_${width}.xml" \
             -hostfile "hostfiles/grid_hostfile_${height},${width}.txt" \
