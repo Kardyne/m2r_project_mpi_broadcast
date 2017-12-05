90ba2fb71a9f53088507ed10efc1490bc2ddd4c9
Alexis Janon <kardyne@gmail.com>
2017-12-05 18:17:35 +0100

    Fixed mpi_allreduce default call

--- src/mpi_allreduce.c
+++ src/mpi_allreduce.c
@@ -88,9 +88,11 @@ int main(int argc, char **argv)
 		ring_allreduce(&mpi_parameters, sum);
 	} else if(!strcmp(arguments.topology, "grid2d")) {
 		grid2d_allreduce(&mpi_parameters, sum);
-	} else if(!mpi_parameters.p_rank) {
-		log_msg(LOG_INFO, "[%s] topology, using MPI default call.",
-			arguments.topology);
+	} else if(!strcmp(arguments.topology, "default")) {
+		if(!mpi_parameters.p_rank)
+			log_msg(LOG_INFO,
+				"[%s] topology, using MPI default call.",
+				arguments.topology);
 		allreduce(&mpi_parameters, MPI_SUM);
 	}
 	MPI_Finalize();
