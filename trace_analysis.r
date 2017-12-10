1246c5174751a7821279eb76f1c2e9e05e2e2c0b
Benjamin Roussillon <Benjamin.Roussillon@ensimag.grenoble-inp.fr>
2017-12-10 11:48:38 +0100

    Modified trace_analysis.r
    The script now analyses the new results and does some linear regressions.

--- trace_analysis.r
+++ trace_analysis.r
@@ -2,7 +2,8 @@ plot_time_comm <- function(topology, bandwidth, latency){
   library(ggplot2)
   nb_procs <- c(16, 256, 625)
   #different sizes in bytes.
-  sizes <- c(4, 10, 16, 64, 100, 256, 1000, 1024, 4096, 10000, 16384)
+  sizes <- c(4, 10, 16, 64, 100, 256, 1000, 1024, 4096, 10000, 16384, 65536,100000,
+             262144,1000000,1048576)
   times_procs_default <- vector("list", 3)
   times_procs_alg <- vector("list", 3)
   
@@ -42,14 +43,34 @@ plot_time_comm <- function(topology, bandwidth, latency){
     color2 = "green"
     y1 = times_procs_default[i]
     y2 = times_procs_alg[i]
-    plot(sizes[8:11], unlist(times_procs_default[i])[8:11],col=color2,ylim=range(c(y1,y2)),
+    plot(sizes[9:16], unlist(times_procs_default[i])[9:16],col=color2,ylim=range(c(y1,y2)),
          main=graph_title, xlab="Message size (bytes)", ylab="Time taken (seconds)",
          sub = sprintf("latency = %d us bandwidth = %d Mb/s", latency, bandwidth))
-    lines(sizes[8:11], unlist(times_procs_default[i])[8:11], type="l",col=color2)
-    points(sizes[8:11], unlist(times_procs_alg[i])[8:11],col=color1)
-    lines(sizes[8:11], unlist(times_procs_alg[i])[8:11], type="l",col=color1)
-    legend(x = "topleft", legend=c("Optimized algorithm", "MPI_ALLREDUCE"),
+    lines(sizes[9:16], unlist(times_procs_default[i])[9:16], type="l",col=color2)
+    points(sizes[9:16], unlist(times_procs_alg[i])[9:16],col=color1)
+    lines(sizes[9:16], unlist(times_procs_alg[i])[9:16], type="l",col=color1)
+    legend(x = "bottomright", legend=c("Optimized algorithm", "MPI_ALLREDUCE"),
            col=c("red", "green"), lty=1:2, cex=0.8)
+    #The multiplicator factor of the processors in hockney model
+    mult_proc = 0
+    if(topology == "ring") {
+      mult_proc = nb_procs[i] - 1
+    }
+    else {
+      mult_proc = 2*sqrt(nb_procs[i]) - 2
+    }
+    print("Linear regression for optimized algorithm for number of processors:")
+    print(nb_procs[i])
+    lmResult <- lm(unlist(times_procs_default[i])[9:12] ~ sizes[9:12])
+    a = unname(coef(lmResult)[2])
+    b = unname(coef(lmResult)[1])
+    print(a)
+    print("Estimated bandwidth on the linear part of the graph")
+    print(8*10^(-6)*mult_proc/a)
+    print("Mb/s")
+    print("Estimated latency on the linear part of the graph")
+    print((10^6)*b/mult_proc)
+    print("us")
   }
   
   #Short messages
@@ -60,13 +81,22 @@ plot_time_comm <- function(topology, bandwidth, latency){
     color2 = "green"
     y1 = times_procs_default[i]
     y2 = times_procs_alg[i]
-    plot(sizes[1:7], unlist(times_procs_default[i])[1:7],col=color2,ylim=range(c(y1,y2)),
+    plot(sizes[1:8], unlist(times_procs_default[i])[1:8],col=color2,ylim=range(c(y1,y2)),
          main=graph_title, xlab="Message size (bytes)", ylab="Time taken (seconds)",
          sub = sprintf("latency = %dus bandwidth = %dMb/s", latency, bandwidth))
-    lines(sizes[1:7], unlist(times_procs_default[i])[1:7], type="l",col=color2)
-    points(sizes[1:7], unlist(times_procs_alg[i])[1:7],col=color1)
-    lines(sizes[1:7], unlist(times_procs_alg[i])[1:7], type="l",col=color1)
+    lines(sizes[1:8], unlist(times_procs_default[i])[1:8], type="l",col=color2)
+    points(sizes[1:8], unlist(times_procs_alg[i])[1:8],col=color1)
+    lines(sizes[1:8], unlist(times_procs_alg[i])[1:8], type="l",col=color1)
     legend(x = "topleft", legend=c("Optimized algorithm", "MPI_ALLREDUCE"),
            col=c("red", "green"), lty=1:2, cex=0.8)
   }
+  
+  #linear regression on a size of message for the numbers of processors
+  time_message_size <- numeric()
+  for(i in 1:3) {
+    time_message_size = c(time_message_size, unlist(times_procs_alg[i])[9])
+  }
+  print("linear regression on a fixed message size for different number of processors")
+  print(lm(time_message_size ~ nb_procs - 1))
+  plot(y = time_message_size, x = nb_procs - 1)
 }
